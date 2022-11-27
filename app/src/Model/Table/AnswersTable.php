69063fac751a8a247b544dbbbd39fb24adc36cf7
Asako Kondo <kondoasako@gmail.com>
2022-11-27 19:17:52 +0900

    8-7 質問と回答の入力値をチェックする

--- app/src/Model/Table/AnswersTable.php
+++ app/src/Model/Table/AnswersTable.php
@@ -3,6 +3,8 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\ORM\RulesChecker;
+use Cake\Validation\Validator;
 
 class AnswersTable extends Table {
     public function initialize(array $config) {
@@ -17,5 +19,29 @@ class AnswersTable extends Table {
             'foreignKey' => 'question_id',
             'joinType' => 'INNER'
         ]);
+    }
+
+    public function validationDefault(Validator $validator) {
+        $validator
+            ->nonNegativeInteger('id', 'IDが不正です')
+            ->allowEmpty('id', 'create', 'IDが不正です');
+        
+        $validator
+            ->scalar('body', '回答内容が不正です')
+            ->requirePresence('body', 'create', '回答内容が不正です')
+            ->notEmpty('body', '回答内容は必ず入力してください')
+            ->maxLength('body', 140, '回答内容は140字以内で入力してください');
+
+        return $validator;
+    }
+
+    public function buildRules(RulesChecker $rules) {
+        $rules->add($rules->existsIn(
+            ['question_id'],
+            'Questions',
+            '質問が既に削除されているため回答することができません'
+        ));
+
+        return $rules;
     }
 }
